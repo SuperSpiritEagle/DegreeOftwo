ca7e1e3ef7bf6d5bcd40238f0291280293731c64
SuperSpiritEagle <118557578+SuperSpiritEagle@users.noreply.github.com>
2023-01-24 12:26:58 +0500

    init

---
+++ DegreeOftwo/DegreeOftwo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DegreeOftwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number = 2;
+            int minNumber = 1;
+            int maxNumber = 100;
+            int randomNumber = 0;
+            int degree = 1;
+            int sumNumber = 1;
+
+            Random random = new Random();
+
+            randomNumber = random.Next(minNumber, maxNumber);
+
+            while (randomNumber >= sumNumber)
+            {
+                
+                sumNumber *= number;
+                degree++;
+            }
+
+            degree--;
+            Console.WriteLine($"Для числа {randomNumber} будет {number} в степени {degree} = {sumNumber}");
+        }
+    }
+}
